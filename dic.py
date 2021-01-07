a21a47eda55ff619d6dd25acd1d1efcd983de1b9
lincoln8085 <57235267+lincoln8085@users.noreply.github.com>
2021-01-07 21:27:22 +0530

    Update dic.py

--- dic.py
+++ dic.py
@@ -5,4 +5,4 @@ this_dic= {"nm1":"sam","nm2":"sim","nm3":"sek"}
 print(this_dic["nm1"])
 print(this_dic.get("nm2"))
 print(this_dic.get("nm3"))
-print(this_dic.get("nm4","NO NAME found "))
+print(this_dic.get("nm4","NO NAME found in this key"))
