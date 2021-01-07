78f75d83bb458eb3279db30029624febdfa5a79a
souravsingh3019 <souravsingh3019@gmail.com>
2021-01-07 21:42:26 +0530

    new files

---
+++ while_test.py
@@ -0,0 +1,21 @@
+#5thjan2021
+
+i=True
+#if changes universal value of var
+if i :
+    i=False
+    print(i)
+else:
+    i=True
+    print(i)
+
+print(i)
+#increment in while loop
+i=1
+
+while   i<5:
+    print(1)
+    i+=1
+    print(i)
+
+print(i)
